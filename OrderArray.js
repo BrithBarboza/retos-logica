979dc54ee51f1146fba0b93fa566ee37c6d466a5
juanncode <juanortizmg@gmail.com>
2022-09-10 12:36:40 -0500

    Resolve challenge

---
+++ OrderArray.js
@@ -0,0 +1,17 @@
+function numberSort(arrayNumber) {
+  let newArray = [...arrayNumber];
+
+  let dataLen = newArray.length;
+  for (let i = 0; i < dataLen; i++) {
+    for (let j = 0; j < dataLen; j++) {
+      if (newArray[j] > newArray[j + 1]) {
+        let nextElement = newArray[j + 1];
+        newArray[j + 1] = newArray[j];
+        newArray[j] = nextElement;
+      }
+    }
+  }
+  return newArray;
+}
+
+console.log(numberSort([10, 2, 0, 5, 4, 3, 0, 1]));
